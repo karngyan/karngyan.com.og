1e6113aea4842c48662a9c1c4567fbe8fafe6b78
Karn <karn@customer.io>
2022-06-02 19:17:36 +0530

    add ir-blr

---
+++ pages/ir-blr/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative max-w-7xl border-r border-l border-dashed border-gray-700 py-6 mx-auto">
+    <div data-aos="fade-up" class="text-center">
+      <h2 class="text-2xl tracking-tight font-extrabold text-gray-200">
+        welcome to InterviewReady's june 2022 meetup
+      </h2>
+      <p class="mt-2 px-2 max-w-2xl mx-auto text-sm leading-7 text-gray-400">
+        launch your own startup
+      </p>
+    </div>
+    <div class="flex items-center justify-center py-16">
+      <iframe src="https://pitch.com/embed/20e3c0f9-0730-4efe-9fac-42fa3edefbdd" allow="fullscreen" allowfullscreen="" class="w-full" height="500" style="border:0"></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
